086c59901dae3d8ef86d969ccc9165ae8cc37b70
ChefBigDev <34831190+ChefBigDev@users.noreply.github.com>
2018-06-12 15:30:36 +0200

    optimized getting tweets
    used to reload on unsupported tweets, uses loop until condition met now.

--- js/app.js
+++ js/app.js
@@ -1,57 +1,65 @@
 function initTweets() {
-        this.getJSON(function(response) {
+    this.getJSON(function(response) {
 
-        let tweets = JSON.parse(response);
-        
-        //Get random integer between 0 and amount of tweets available.
-        let random_integer = Math.floor(Math.random() * (tweets.length - 0) + 0);
-        let tweet = tweets[random_integer].value;
-        const tweetUrl = tweets[random_integer].href;
-        //Turn tweet into array to work with.
-        let tweet_stripped = tweet.split(" ");
+    let tweets = JSON.parse(response);
+    //Get random integer between 0 and amount of tweets available.
+    let random_integer = Math.floor(Math.random() * (tweets.length - 0) + 0);
+    let tweet = tweets[random_integer].value;
+    let tweetUrl = tweets[random_integer].href;
+    //Turn tweet into array to work with.
+    let tweet_stripped = tweet.split(" ");
 
-        //Certain types of tweets dont function as of yet, page will reload until it finds a working tweet.
-        //Will be fixed at some point by not including them in JSON data.
+    //If unsupported tweet, get a new one until its valid.
+    let valid_tweet = false;
+    while(!valid_tweet) {
         if(tweet_stripped[0].startsWith("\ndril\n@") || tweet_stripped[0].startsWith("\n\n")){
-            location.reload();
-        } else {
-            let tweet_final = "";
-
-            //Loop through tweet, generate finalized tweet string.
-            for(let i = 0; i < tweet_stripped.length - 4; i++){
-                //Remove "Dril" from the start of every tweet, add nbsp.
-                if(i == 0) {
-                    tweet_final += tweet_stripped[i].substr(5) + " ";
-                }
-                //Remove month from last word in tweet.
-                else if(i == tweet_stripped.length - 5){
-                    tweet_final += tweet_stripped[i].substring(0, tweet_stripped[i].length - 3);
-                }
-                //Else just append to tweet_final and add nbsp.
-                else {
-                    tweet_final += tweet_stripped[i].replace(/(\r\n\t|\n|\r\t)/gm,"") + " ";
-                }
-            }
-            //Create month
-            const month = tweet_stripped[tweet_stripped.length-5];
-            const month_stripped = month.replace(/(\r\n\t|\n|\r\t)/gm,"");
-            const month_final = month.slice(-3);
-            //Create day
-            const day = tweet_stripped[tweet_stripped.length-4];
-            const day_final = day.replace(",","");
-            //Create year 
-            const year_final = tweet_stripped[tweet_stripped.length-3];
-            //Create attribute with date info and tweet reference
-            const attr = document.createElement('a');
-            attr.setAttribute('href', tweetUrl);
-            attr.setAttribute('target', '_blank');
-            attr.innerHTML = month_final + " " + day_final + " " + year_final;
-            //Append tweet and date to target div
-            let tweetTarget = document.getElementById("tweet_target");
-            tweetTarget.innerHTML = tweet_final + " ";
-            tweetTarget.appendChild(attr);
+            //Get random integer between 0 and amount of tweets available.
+            random_integer = Math.floor(Math.random() * (tweets.length - 0) + 0);
+            tweet = tweets[random_integer].value;
+            tweetUrl = tweets[random_integer].href;
+            //Turn tweet into array to work with.
+            tweet_stripped = tweet.split(" ");
+        } else{
+            valid_tweet = true;
         }
+    }
 
+    let tweet_final = "";
+
+    //Loop through tweet, generate finalized tweet string.
+    for(let i = 0; i < tweet_stripped.length - 4; i++){
+        //Remove "Dril" from the start of every tweet, add nbsp.
+        if(i == 0) {
+            tweet_final += tweet_stripped[i].substr(5) + " ";
+        }
+        //Remove month from last word in tweet.
+        else if(i == tweet_stripped.length - 5){
+            tweet_final += tweet_stripped[i].substring(0, tweet_stripped[i].length - 3);
+        }
+        //Else just append to tweet_final and add nbsp.
+        else {
+            tweet_final += tweet_stripped[i].replace(/(\r\n\t|\n|\r\t)/gm,"") + " ";
+        }
+    }
+
+    //Create month
+    const month = tweet_stripped[tweet_stripped.length-5];
+    const month_stripped = month.replace(/(\r\n\t|\n|\r\t)/gm,"");
+    const month_final = month.slice(-3);
+    //Create day
+    const day = tweet_stripped[tweet_stripped.length-4];
+    const day_final = day.replace(",","");
+    //Create year 
+    const year_final = tweet_stripped[tweet_stripped.length-3];
+    //Create attribute with date info and tweet reference link
+    const attr = document.createElement('a');
+    attr.setAttribute('href', tweetUrl);
+    attr.setAttribute('target', '_blank');
+    attr.innerHTML = month_final + " " + day_final + " " + year_final;
+    //Append tweet and date to target div
+    let tweetTarget = document.getElementById("tweet_target");
+    tweetTarget.innerHTML = tweet_final + " ";
+    tweetTarget.appendChild(attr);
     });
 }
 
