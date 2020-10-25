4aecfeb346c2c77e9cddd2847c17efb67cd813ee
Joshua van Houts <joshuavanhouts@hotmail.com>
2020-10-25 19:08:12 +0100

    update

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@ let previousPos = 0;
 let currentPos = 0;
 let currentDirection;
 
+const nameDivs = document.querySelectorAll('.name');
+const rainbowColors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#2E2B5F', '#8B00FF'];
+let currentColor = 0;
+
+const colorInterval = setInterval(() => {
+    nameDivs.forEach(name => {
+       name.style.webkitTextStrokeColor = rainbowColors[currentColor];
+    });
+
+    currentColor++;
+
+    if(currentColor == rainbowColors.length) {
+        currentColor = 0;
+    }
+}, 500);
+
+
 // Reset cube animation if no scroll detected, checked every second
 const textInterval = setInterval(() => {
     if(currentPos  == previousPos) {
