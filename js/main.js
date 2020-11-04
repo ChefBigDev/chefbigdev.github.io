e03b55eaee8871a885be23bec703139195a6bcd4
Joshua <ravioliboy@yahoo.com>
2020-11-04 10:25:18 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ let currentColor = 0;
 const colorInterval = setInterval(() => {
     nameDivs.forEach(name => {
        name.style.webkitTextStrokeColor = rainbowColors[currentColor];
+        particles.particles.color.value = rainbowColors[currentColor];
     });
 
     currentColor++;
@@ -60,3 +61,121 @@ document.body.onscroll = () => {
     }
     lastScrollTop = st <= 0 ? 0 : st;
 };
+
+const particles = {
+    "fpsLimit": 30,
+    "particles": {
+        "number": {
+            "value": 25,
+            "density": {
+                "enable": true,
+                "value_area": 1000
+            }
+        },
+        "color": {
+            "value": rainbowColors,
+            "animation": {
+                "enable": false,
+                "speed": 10,
+                "sync": true
+            }
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0
+            },
+            "polygon": {
+                "nb_sides": 6
+            },
+        },
+        "opacity": {
+            "value": 0.35,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 3,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 10,
+    "random": true,
+    "anim": {
+    "enable": false,
+        "speed": 10,
+        "size_min": 0.1,
+        "sync": false
+}
+},
+"links": {
+    "enable": false,
+        "distance": 100,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+},
+"move": {
+    "enable": true,
+        "speed": 6,
+        "direction": "down",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+        "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+    }
+}
+},
+"interactivity": {
+    "detect_on": "canvas",
+        "events": {
+        "onhover": {
+            "enable": false,
+                "mode": "repulse"
+        },
+        "onclick": {
+            "enable": false,
+                "mode": "push"
+        },
+        "resize": true
+    },
+    "modes": {
+        "grab": {
+            "distance": 400,
+                "line_linked": {
+                "opacity": 1
+            }
+        },
+        "bubble": {
+            "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 0.8
+        },
+        "repulse": {
+            "distance": 200
+        },
+        "push": {
+            "particles_nb": 4
+        },
+        "remove": {
+            "particles_nb": 2
+        }
+    }
+},
+"retina_detect": true,
+    "background": {
+    "color": "#ffffff",
+        "image": "",
+        "position": "50% 50%",
+        "repeat": "no-repeat",
+        "size": "cover"
+}
+};
+
+//Particles
+tsParticles.load('tsparticles', particles);
