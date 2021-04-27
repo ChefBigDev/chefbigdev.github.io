962208aaf2b810d366fef561cf9bc03ef16ea636
Jxshvh <ravioliboy@yahoo.com>
2021-04-27 21:03:04 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const particles = {"fpsLimit":30,"particles":{"number":{"value":10,"density":{"enable":true,"value_area":1000}},"color":{"value":"#ffffff","animation":{"enable":false,"speed":10,"sync":true}},"shape":{"type":"circle","stroke":{"width":0},"polygon":{"nb_sides":6}},"opacity":{"value":1,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"random":true,"anim":{"enable":true,"speed":2,"size_min":1,"size_max":3,"sync":false}},"links":{"enable":false,"distance":100,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":3,"direction":"down","random":false,"straight":false,"out_mode":"out","attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":0.8},"repulse":{"distance":200},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true,"background":{"color":"#171827","image":"","position":"50% 50%","repeat":"no-repeat","size":"cover"}}
 let rellax = new Rellax('.rellax');
 let cube = document.querySelector('.cube');
 let previousPos = 0;
@@ -6,27 +7,23 @@ let currentDirection;
 
 const nameDivs = document.querySelectorAll('.name');
 const rainbowColors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#2E2B5F', '#8B00FF'];
-const middleLink = document.getElementById('middleLink');
+//const middleLink = document.getElementById('middleLink');
 let currentColor = 0;
 
 const modeSwitchInput = document.querySelector('.modeSwitchInput');
 const modeSwitchLabel = document.querySelector('.modeSwitchLabel');
 
+modeHandler(modeSwitchInput);
 modeSwitchInput.addEventListener('click', modeHandler);
 
-const particles = {"fpsLimit":30,"particles":{"number":{"value":10,"density":{"enable":true,"value_area":1000}},"color":{"value":"#ffffff","animation":{"enable":false,"speed":10,"sync":true}},"shape":{"type":"circle","stroke":{"width":0},"polygon":{"nb_sides":6}},"opacity":{"value":1,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"random":true,"anim":{"enable":true,"speed":2,"size_min":1,"size_max":3,"sync":false}},"links":{"enable":false,"distance":100,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":3,"direction":"down","random":false,"straight":false,"out_mode":"out","attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":0.8},"repulse":{"distance":200},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true,"background":{"color":"#171827","image":"","position":"50% 50%","repeat":"no-repeat","size":"cover"}}
-
-function modeHandler() {
-    if(this.checked) {//Dark mode enabled
+function modeHandler(e) {
+    if(this.checked || e.checked) {//Dark mode enabled
         //Switch styling
         modeSwitchLabel.innerHTML = 'LIGHT';
         modeSwitchLabel.style.color = 'white';
         //Element styling
-        document.querySelector('.middle').style.background = '#171827';
-        document.querySelector('.middle').style.borderTop = '2px solid #474755';
-        document.querySelector('.middle').style.borderBottom = '2px solid #474755';
+        document.querySelector('.bottom').style.background = '#171827';
         document.querySelector('.primary').style.color = 'white';
-        document.querySelector('.aboutme').style.color = 'white';
         document.body.style.background = '#171827';
 
         particles.background.color = '#171827';
@@ -38,10 +35,9 @@ function modeHandler() {
         modeSwitchLabel.style.color = '#171827';
         //Element styling
         document.querySelector('.primary').style.color = '';
-        document.querySelector('.aboutme').style.color = '';
-        document.querySelector('.middle').style.background = '';
-        document.querySelector('.middle').style.border = 'none';
-        document.body.style.background = 'white';
+        document.querySelector('.bottom').style.background = '';
+        
+        document.body.style.background = '#f1f1f1';
 
         document.getElementById('tsparticles').innerHTML = '';
     }
@@ -73,7 +69,7 @@ function resetAnimation() {
     cube.style.transform = 'translateZ(-100px) rotateY(0deg)';
     currentDirection = '';
     currentPos = 0;
-    middleLink.style.marginTop = '0px';
+    //middleLink.style.marginTop = '0px';
 }
 
 // set initial side
@@ -103,7 +99,7 @@ document.body.onscroll = () => {
     if (st > lastScrollTop){//bottom
 
         // middleLink.style.marginTop = '10vh';
-        middleLink.style.color = getRandomRainbowColor();
+        //middleLink.style.color = getRandomRainbowColor();
 
         if(currentDirection == 'top') {
             currentPos = currentPos - 2;
@@ -113,7 +109,7 @@ document.body.onscroll = () => {
         cube.style.transform = 'translateZ(-100px) rotateY( '+ currentPos +'deg)';
     } else {//top
         //middleLink.style.marginTop = '-10vh';
-        middleLink.style.color = getRandomRainbowColor();
+        //middleLink.style.color = getRandomRainbowColor();
 
         if(currentDirection == 'bottom') {
             currentPos = currentPos + 2;
